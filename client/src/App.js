96ef96b1bdea85b3d2d6c3c8e42f2ddffe1c0e9b
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-06-22 22:53:21 -0400

    minor changes to routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Signup from "./screens/signup/Signup";
 import Login from "./screens/login/Login";
+import Home from "./screens/home/Home";
+import Favorites from "./screens/favorites/Favorites";
+import About from "./screens/about/about";
+import User from "./screens/user/User";
 
 function App() {
   return (
@@ -12,6 +16,18 @@ function App() {
         </Route>
         <Route exact path="/login">
           <Login />
+        </Route>
+        <Route exact path="/bevra-list">
+          <Home />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/about-us">
+          <About />
+        </Route>
+        <Route exact path="/user-profile">
+          <About />
         </Route>
       </Switch>
     </div>
