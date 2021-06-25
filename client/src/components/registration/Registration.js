b32623de15fe4e7748a44aa17e474e2ae87908f6
David Espinal <despinal0425@gmail.com>
2021-06-25 15:30:12 -0400

    added the input for username and password. Also implemented the submit function

--- client/src/components/registration/Registration.js
+++ client/src/components/registration/Registration.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from "axios";
 
 export default function Registration() {
+  
+  //hooks to capture user inputs
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  
+  
+  
+  const register = () => {
+    axios({
+      method: "POST",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:5000/api/register",
+    }).then((res) => console.log(res));
+  }
+
+
+  
   return (
     <div>
-      
+      <input
+        placeholder="username"
+        onChange={ (e) => setRegisterUsername(e.target.value)}
+      />
+      <input
+        placeholder="password"
+        onChange={ (e) => setRegisterPassword(e.target.value)}
+      />
+
+      <button >Sign In</button>
     </div>
   )
 }
