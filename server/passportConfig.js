07718f0ae68b36c6af38465393a6f4a1d3f54943
David Espinal <despinal0425@gmail.com>
2021-06-25 10:16:40 -0400

    added a .env file with the link to my mongodb database

--- server/passportConfig.js
+++ server/passportConfig.js
@@ -8,4 +8,12 @@ const bcrypt = require('require');
 const localStrategy = require('passport-local').Strategy;
 
 
-//---  ---
+//--- function below will use localStrategy to help find a matching username and password in the User database ---
+
+
+
+
+//--- Passport Serializer Here: user is put inside express session ---
+
+
+//--- Passport Deserializer Here: grab user outside of express session ---
