6c01c2aa0f3b940947c83a3611671f1d8ff2565a
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-06-28 19:29:43 -0400

    installed tailwind files, bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import Home from "../src/screens/home/";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
