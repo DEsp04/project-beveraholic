9b903277a84b52fb6df36bd72d356ef67a6d2675
Shan Siddiqui <shansiddiqui@Shans-Air.lan>
2021-06-28 19:44:19 -0400

    added text to home, and pasted code into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,49 @@
 import "./App.css";
-import Home from "../src/screens/home/";
+import { Route, Switch, useHistory } from "react-router-dom";
+import Signup from "./screens/signup/Signup";
+import Login from "./screens/login/Login";
+import Home from "./screens/home/Home";
+import Favorites from "./screens/favorites/Favorites";
+import About from "./screens/about/About";
+import User from "./screens/user/User";
+import { useEffect, useState } from "react";
+
 function App() {
+  const history = useHistory();
+
+  const ayman = localStorage.getItem("userInfo");
+  console.log("this is from app.js:", ayman);
+
+  useEffect(() => {
+    if (ayman) {
+      history.push("/home");
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Home />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Signup />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/favorites">
+            <Favorites />
+          </Route>
+          <Route exact path="/about-us">
+            <About />
+          </Route>
+          <Route exact path="/user-profile">
+            <User />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
--- client/src/screens/home/home.js
+++ client/src/screens/home/home.js
@@ -1,5 +1,11 @@
 import React from "react";
+import Layout from "../../components/layout/Layout";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <h2>Hello World</h2>
+    </Layout>
+  );
 }
