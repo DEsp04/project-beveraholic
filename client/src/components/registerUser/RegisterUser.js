0a8b67dbdc73e0bef46de6e27ddcc4471b905413
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-06-28 20:18:57 -0400

    fixed client signup imports

--- client/src/components/registerUser/RegisterUser.js
+++ client/src/components/registerUser/RegisterUser.js
@@ -1,14 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Registration() {
-  
   //hooks to capture user inputs
   const [registerUsername, setRegisterUsername] = useState("");
   const [registerPassword, setRegisterPassword] = useState("");
-  
-  
-  
+
   const register = () => {
     axios({
       method: "POST",
@@ -19,22 +16,24 @@ export default function Registration() {
       withCredentials: true,
       url: "http://localhost:5000/api/register",
     }).then((res) => console.log(res));
-  }
+  };
 
-
-  
   return (
     <div>
       <input
-        placeholder="username"
-        onChange={ (e) => setRegisterUsername(e.target.value)}
+        placeholder="Username"
+        onChange={(e) => setRegisterUsername(e.target.value)}
       />
       <input
-        placeholder="password"
-        onChange={ (e) => setRegisterPassword(e.target.value)}
+        placeholder="Email Address"
+        onChange={(e) => setRegisterUsername(e.target.value)}
+      />
+      <input
+        placeholder="Password"
+        onChange={(e) => setRegisterPassword(e.target.value)}
       />
 
-      <button onClick={register}>Sign In</button>
+      <button onClick={register}>Sign Up</button>
     </div>
-  )
+  );
 }
