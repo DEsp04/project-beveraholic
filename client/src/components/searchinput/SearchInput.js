9e0a9b864f5b2d586652965065b653ce19830f7f
David Espinal <despinal0425@gmail.com>
2021-07-16 17:57:59 -0400

    added border to the search input

--- client/src/components/searchinput/SearchInput.js
+++ client/src/components/searchinput/SearchInput.js
@@ -27,25 +27,24 @@ export default function SearchInput() {
 
   return (
     <div>
-      <label
-        htmlFor="email"
-        className="block text-sm font-medium text-gray-700"
-      >
-        Search Beverages
-      </label>
       <div className="mt-1 flex rounded-md shadow-sm">
         <div className="relative flex items-stretch flex-grow focus-within:z-10">
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+
+
+
             <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
           </div>
           <input
             type="text"
             name="searchInput"
             id="searchInput"
-            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300"
+            className="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-full pl-10 sm:text-sm border-gray-300"
             placeholder="Search"
           />
 
+          
+          
           <Menu as="div" className="relative inline-block text-left">
             {({ open }) => (
               <>
