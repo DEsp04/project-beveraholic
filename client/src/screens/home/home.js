0eb7c0679a00f81c66a542053e1dddab5bd0d923
David Espinal <despinal0425@gmail.com>
2021-07-20 16:15:15 -0400

    added the full height elements to the recipes and home page

--- client/src/screens/home/home.js
+++ client/src/screens/home/home.js
@@ -3,6 +3,7 @@ import Layout from "../../components/layout/Layout";
 import SearchInput from "../../components/searchinput/SearchInput";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchLoadUser } from "../../redux/loadUserSlice";
+import FullHeight from "react-full-height";
 
 export default function Home() {
 
@@ -30,17 +31,19 @@ export default function Home() {
 
   return (
     <Layout>
-      <main className="py-10">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          { userName() }
-        </div>
-        <div className="max-w-7xl mx-auto mt-10 sm:px-6 lg:px-8" >
-          <SearchInput />
-          {/* <div className="px-4 py-8 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-          </div> */}
-        </div>
-      </main>
+      <FullHeight>
+        <main className="py-10">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            { userName() }
+          </div>
+          <div className="max-w-7xl mx-auto mt-10 sm:px-6 lg:px-8" >
+            <SearchInput />
+            {/* <div className="px-4 py-8 sm:px-0">
+              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+            </div> */}
+          </div>
+        </main>
+      </FullHeight>
     </Layout>
   );
 }
