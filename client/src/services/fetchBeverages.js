a034a0a444082bd2c1a5e75bdb248faae5063bad
David Espinal <despinal0425@gmail.com>
2021-07-21 20:25:43 -0400

    created file to make beverage component

--- client/src/services/fetchBeverages.js
+++ client/src/services/fetchBeverages.js
@@ -3,6 +3,9 @@ import axios from "axios";
 export const userUrl = "https://beveraholic.herokuapp.com/api/user";
 export const beveragelUrl = "https://beveraholic.herokuapp.com/api/beverages";
 
+
+
+
 export const getBeverages = async () => {
   const response = await axios.get(beveragelUrl).then(({ data }) => {
     return data;
