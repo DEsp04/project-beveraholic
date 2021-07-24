a6b1102b531565149104dcfd3e0d87c7ba7c7ba8
David Espinal <despinal0425@gmail.com>
2021-07-24 18:16:53 -0400

    form element and input for beverage name

--- client/src/components/addNewRecipe/NewRecipe.js
+++ client/src/components/addNewRecipe/NewRecipe.js
@@ -17,7 +17,7 @@ export default function Modal() {
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl bg-shark-500 rounded-md">
+            <div className="relative w-auto my-6 mx-auto max-w-md bg-shark-500 rounded-md w-full">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
@@ -26,42 +26,53 @@ export default function Modal() {
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span className="text-xxxl bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none text-spring-wood-500">
                       ×
                     </span>
                   </button>
-                  </div>
-                  <h3 className="text-xxxl font-extrabold text-spring-wood-500">
+                </div>
+                
+                <form className="space-y-6" action="#" method="POST">
+                  <h3 className="text-xxxl font-extrabold text-spring-wood-500 text-center">
                     Add New Recipe
                   </h3>
                 
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
+                  <div className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-spring-wood-500"
+                    >
+                      Name
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        id="name"
+                        name="name"
+                        type="name"
+                        required
+                        className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                        // onChange={(e) => setLoginEmail(e.target.value)}
+                      />
+                    </div>  
+                  </div>
+                    
+
+
                 </div>
+                  
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
+                  <div className="flex items-center justify-center p-6 rounded-b">
+                    <button
+                      className="w-1/3 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-xl font-bold font-medium text-spring-wood-500 bg-vin-rouge-500 hover:bg-vin-rouge-700 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-indigo-500"
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Add
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
