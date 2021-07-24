d7c868f7d2a12116c32ee280cafa6b6d79ccbc28
David Espinal <despinal0425@gmail.com>
2021-07-24 15:22:28 -0400

    working on header of the modul

--- client/src/components/addNewRecipe/NewRecipe.js
+++ client/src/components/addNewRecipe/NewRecipe.js
@@ -17,14 +17,11 @@ export default function Modal() {
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl bg-shark-500 rounded-md">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
+                <div className="flex items-start justify-between pt-4 mr-3  rounded-t">
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -33,7 +30,11 @@ export default function Modal() {
                       ×
                     </span>
                   </button>
-                </div>
+                  </div>
+                  <h3 className="text-xxxl font-extrabold text-spring-wood-500">
+                    Add New Recipe
+                  </h3>
+                
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
