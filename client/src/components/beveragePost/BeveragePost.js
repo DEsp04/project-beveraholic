04bcc4f26d047c34d17404956665cd9f907518f6
David Espinal <despinal0425@gmail.com>
2021-07-26 17:56:39 -0400

    created beverage information page

--- client/src/components/beveragePost/BeveragePost.js
+++ client/src/components/beveragePost/BeveragePost.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { NavLink } from "react-router-dom";
 
 
 
@@ -7,44 +7,41 @@ export default function BeveragePost(props) {
   console.log(props)
   
   return (
-    <div className="text-center rounded-md px-10 m-1 p-2 bg-shark-500 md:mx-10 mx-0">
+    <div className="text-center rounded-md px-10 m-1 p-2 bg-shark-500 md:mx-10 mx-0 w-auto">
       <div>
         <img
-           className="h-40 w-full rounded-none 100%"
+           className="h-40 w-full rounded-md 100%"
           src={props.beverage_image}
           alt=""
         />
       </div>
-      <h3>{props.beverage_name}</h3>
-      <h2>{props.alcohol_content}</h2>
+      <h3 className="text-spring-wood-500 text-base font-semibold my-3 overflow-hidden whitespace-nowrap overflow-ellipsis w-20">{props.beverage_name}</h3>
+      <h2 className="text-spring-wood-500">{props.alcohol_content}</h2>
       
 
       <div className="flex items-center justify-center p-6 pt-2 rounded-b">
-              {/* <NavLink
+              <NavLink
         to={{
-          pathname: "/jobDescription",
-          state: props
-            ? {
-                logo: props.company_logo_url,
-                title: props.title,
-                company: props.company_name,
-                category: props.category,
-                salary: props.salary,
-                requirement: props.candidate_required_location,
-                hours: props.job_type,
-                description: props.description,
-                apply: props.url,
+          pathname: "/beverageInfo",
+            state: props
+              ? {
+                name: props.beverage_name,
+                image: props.beverage_image,
+                category: props.beverage_category,
+                content: props.alcohol_content,
+                instruction: props.instruction,
+                ingredient: props.ingredients,
               }
             : {},
         }}
-      > */}
+      >
         <button
           type="button"
           className = " flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-x font-bold font-medium text-spring-wood-500 bg-vin-rouge-500 hover:bg-vin-rouge-700 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-indigo-500"
         >
           View
         </button>
-        {/* </NavLink>   */}
+        </NavLink>  
       </div>  
     </div>
   )
