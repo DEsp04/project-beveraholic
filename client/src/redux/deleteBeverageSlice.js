fb5b5321b551bb91a7db3dbf730f75b00759de6c
David Espinal <despinal0425@gmail.com>
2021-07-27 19:35:05 -0400

    created the delete item slice

--- client/src/redux/deleteBeverageSlice.js
+++ client/src/redux/deleteBeverageSlice.js
@@ -1,2 +1,45 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { deleteDrink } from "../services/deleteBeverage";
+
+
+export const deleteBeverage = createAsyncThunk(
+  "deleteItem/ deleteBeverage",
+  async ({ id }) => {
+    
+    console.log(id);
+
+    // const data = deleteDrink({ id })
+    
+    // console.log(data)
+
+    return data;
+  }
+);
+
+
+const deleteSlice = createSlice({
+  name: "deleteItem",
+  initialState: {
+    status: null,
+    deletedDrink: null,
+  },
+
+
+  extraReducers: {
+    [deleteBeverage.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [deleteBeverage.fulfilled]: (state, {payload}) => {
+      state.status = "success";
+      state.deletedDrink = payload;
+    },
+    [deleteBeverage.rejected]: (state, action) => {
+      state.status = "failed";
+    },
+
+  },
+
+
+})
+
+export default  deleteSlice.reducer;
