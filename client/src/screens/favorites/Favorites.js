643d90b66af7894a40ba3c4c53705716d44e79b5
David Espinal <despinal0425@gmail.com>
2021-07-31 13:23:14 -0400

    favorite list shows

--- client/src/screens/favorites/Favorites.js
+++ client/src/screens/favorites/Favorites.js
@@ -49,8 +49,10 @@ useEffect(()=>{
             <div className="px-4 py-8 sm:px-0">
               <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
           </div>
+          <div className="flex justify-start items-center flex-wrap mb-16">
           { renderFavoriteList()}
-            {/* /End replace */}
+          {/* /End replace */}
+          </div>
           </div>
         </main>
     </Layout>
